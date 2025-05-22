2f729ee0c4369a10947a78d1969b4899936c7252
amingamal <amingamal@hotmail.com>
2025-05-22 20:37:22 +0200

    Init BambooCardDiscounts plugin

---
+++ src/Plugins/Nop.Plugin.DiscountRules.BambooCardDiscounts/BambooCardDiscounts.cs
@@ -0,0 +1,11 @@
+using Nop.Services.Plugins;
+
+namespace Nop.Plugin.DiscountRules.BambooCardDiscounts
+{
+    /// <summary>
+    /// Rename this file and change to the correct type
+    /// </summary>
+    public class BambooCardDiscounts : BasePlugin
+    {
+    }
+}
