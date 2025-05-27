a85f5603da3e3f61b46424bec63a81f6b19b8a4f
amingamal <amingamal@hotmail.com>
2025-05-27 21:53:47 +0200

    Add gift message column to order table

---
+++ src/Plugins/Nop.Plugin.Widgets.CheckoutGiftMessage/Data/Builders/BambooOrderBuilder.cs
@@ -0,0 +1,14 @@
+﻿using FluentMigrator.Builders.Create.Table;
+using Nop.Data.Mapping.Builders;
+using Nop.Plugin.Widgets.CheckoutGiftMessage.Domains;
+
+namespace Nop.Plugin.Widgets.CheckoutGiftMessage.Data.Builders
+{
+    internal class BambooOrderBuilder : NopEntityBuilder<BambooOrder>
+    {
+        public override void MapEntity(CreateTableExpressionBuilder table)
+        {
+            table.WithColumn(nameof(BambooOrder.GiftMessage)).AsString(int.MaxValue).Nullable();
+        }
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Widgets.CheckoutGiftMessage/Data/Migrations/AddGiftMessageColumnToOrderMigration.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+using Nop.Core.Domain.Orders;
+using Nop.Data.Migrations;
+using Nop.Plugin.Widgets.CheckoutGiftMessage.Domains;
+
+namespace Nop.Plugin.Widgets.CheckoutGiftMessage.Data.Migrations
+{
+    [NopMigration("2025/05/27 17:20:00", "Add Gift message column to order table", MigrationProcessType.Installation)]
+    public class AddGiftMessageColumnToOrderMigration : Migration
+    {
+        public override void Down()
+        {
+            if (Schema.Table(nameof(Order)).Column(nameof(BambooOrder.GiftMessage)).Exists())
+                Delete.Column(nameof(BambooOrder.GiftMessage)).FromTable(nameof(Order));
+        }
+
+        public override void Up()
+        {
+            if (!Schema.Table(nameof(Order)).Column(nameof(BambooOrder.GiftMessage)).Exists())
+                Alter.Table(nameof(Order)).AddColumn(nameof(BambooOrder.GiftMessage)).AsString(int.MaxValue).Nullable();
+        }
+    }
+}
