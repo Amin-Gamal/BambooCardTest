9de66bfcfa5f28ddb12d0a530533c8e887d393e3
amingamal <amingamal@hotmail.com>
2025-05-29 11:15:37 +0200

    Refactor gift message setting key to use Defaults class

--- src/Plugins/Nop.Plugin.Widgets.CheckoutGiftMessage/CheckoutGiftMessagePlugin.cs
+++ src/Plugins/Nop.Plugin.Widgets.CheckoutGiftMessage/CheckoutGiftMessagePlugin.cs
@@ -50,14 +50,14 @@ namespace Nop.Plugin.Widgets.CheckoutGiftMessage
             await _checkoutAttributeService.InsertAttributeAsync(giftMessageCheckoutAttribute);
 
             //save gift message checkout attribute in settings
-            await _settingService.SetSettingAsync("BambooCard-GiftMessage-CheckoutAttribute-Id", giftMessageCheckoutAttribute.Id);
+            await _settingService.SetSettingAsync(Defaults.GIFT_CARD_CHECKOUT_ATTRIBUTE_SETTING_KEY, giftMessageCheckoutAttribute.Id);
 
             await base.InstallAsync();
         }
 
         public override async Task UninstallAsync()
         {
-            int giftMessageCheckoutAttributeId = await _settingService.GetSettingByKeyAsync<int>("BambooCard-GiftMessage-CheckoutAttribute-Id");
+            int giftMessageCheckoutAttributeId = await _settingService.GetSettingByKeyAsync<int>(Defaults.GIFT_CARD_CHECKOUT_ATTRIBUTE_SETTING_KEY);
 
             if (giftMessageCheckoutAttributeId == 0)
                 return;
---
+++ src/Plugins/Nop.Plugin.Widgets.CheckoutGiftMessage/Defaults.cs
@@ -0,0 +1,7 @@
+﻿namespace Nop.Plugin.Widgets.CheckoutGiftMessage
+{
+    public static class Defaults
+    {
+        public const string GIFT_CARD_CHECKOUT_ATTRIBUTE_SETTING_KEY = "BambooCard-GiftMessage-CheckoutAttribute-Id";
+    }
+}
