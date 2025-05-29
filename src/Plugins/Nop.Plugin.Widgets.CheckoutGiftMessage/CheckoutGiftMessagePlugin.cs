a488b433f2d3699521c208b97bbf78a316d43e6d
amingamal <amingamal@hotmail.com>
2025-05-29 09:13:34 +0200

    Add gift message as a CheckoutAttribute during plugin installation

--- src/Plugins/Nop.Plugin.Widgets.CheckoutGiftMessage/CheckoutGiftMessagePlugin.cs
+++ src/Plugins/Nop.Plugin.Widgets.CheckoutGiftMessage/CheckoutGiftMessagePlugin.cs
@@ -1,24 +1,75 @@
+using Nop.Core.Domain.Catalog;
+using Nop.Core.Domain.Orders;
+using Nop.Plugin.Widgets.CheckoutGiftMessage.Components;
+using Nop.Services.Attributes;
 using Nop.Services.Cms;
+using Nop.Services.Configuration;
 using Nop.Services.Plugins;
+using Nop.Web.Framework.Infrastructure;
 
 namespace Nop.Plugin.Widgets.CheckoutGiftMessage
 {
     public class CheckoutGiftMessagePlugin : BasePlugin, IWidgetPlugin
     {
-        public CheckoutGiftMessagePlugin()
+        private readonly IAttributeService<CheckoutAttribute, CheckoutAttributeValue> _checkoutAttributeService;
+        private readonly ISettingService _settingService;
+
+        public CheckoutGiftMessagePlugin(IAttributeService<CheckoutAttribute, CheckoutAttributeValue> checkoutAttributeService, ISettingService settingService)
         {
+            _checkoutAttributeService = checkoutAttributeService;
+            _settingService = settingService;
         }
 
         public bool HideInWidgetList => false;
 
         public Type GetWidgetViewComponent(string widgetZone)
         {
-            throw new NotImplementedException();
+            return typeof(WidgetGiftMessageViewComponent);
         }
 
-        public Task<IList<string>> GetWidgetZonesAsync()
+        public async Task<IList<string>> GetWidgetZonesAsync()
         {
-            throw new NotImplementedException();
+            List<string> widgetZones = new List<string>
+            {
+                PublicWidgetZones.OrderSummaryCartFooter
+            };
+
+            return widgetZones;
+        }
+
+        public override async Task InstallAsync()
+        {
+            CheckoutAttribute giftMessageCheckoutAttribute = new CheckoutAttribute
+            {
+                Name = "Gift Message",
+                TextPrompt = "Enter your gift message here",
+                AttributeControlType = AttributeControlType.MultilineTextbox,
+                DisplayOrder = 1,
+            };
+
+            await _checkoutAttributeService.InsertAttributeAsync(giftMessageCheckoutAttribute);
+
+            //save gift message checkout attribute in settings
+            await _settingService.SetSettingAsync("BambooCard-GiftMessage-CheckoutAttribute-Id", giftMessageCheckoutAttribute.Id);
+
+            await base.InstallAsync();
+        }
+
+        public override async Task UninstallAsync()
+        {
+            int giftMessageCheckoutAttributeId = await _settingService.GetSettingByKeyAsync<int>("BambooCard-GiftMessage-CheckoutAttribute-Id");
+
+            if (giftMessageCheckoutAttributeId == 0)
+                return;
+
+            var giftMessageCheckoutAttribute = await _checkoutAttributeService.GetAttributeByIdAsync(giftMessageCheckoutAttributeId);
+
+            if (giftMessageCheckoutAttribute == null)
+                return;
+
+            await _checkoutAttributeService.DeleteAttributeAsync(giftMessageCheckoutAttribute);
+
+            await base.UninstallAsync();
         }
     }
 }
