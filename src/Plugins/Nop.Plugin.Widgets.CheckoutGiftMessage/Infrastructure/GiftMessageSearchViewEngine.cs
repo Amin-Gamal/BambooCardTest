89472c3092279ba5e444f4848eaaf7525bc1266b
amingamal <amingamal@hotmail.com>
2025-05-29 23:47:30 +0200

    override one page checkout in order to add gift message as a step

---
+++ src/Plugins/Nop.Plugin.Widgets.CheckoutGiftMessage/Infrastructure/GiftMessageSearchViewEngine.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace Nop.Plugin.Widgets.CheckoutGiftMessage.Infrastructure
+{
+    public class GiftMessageSearchViewEngine : IViewLocationExpander
+    {
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+        {
+            if (context.ViewName == "OnePageCheckout")
+                viewLocations = new[] { "~/Plugins/Widgets.CheckoutGiftMessage/Views/Checkout/OnePageCheckout.cshtml" }.Concat(viewLocations);
+
+            return viewLocations;
+        }
+
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+        }
+    }
+}
--- src/Plugins/Nop.Plugin.Widgets.CheckoutGiftMessage/Infrastructure/NopStartup.cs
+++ src/Plugins/Nop.Plugin.Widgets.CheckoutGiftMessage/Infrastructure/NopStartup.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Nop.Core.Infrastructure;
+using Nop.Plugin.Widgets.CheckoutGiftMessage.Controllers;
 using Nop.Plugin.Widgets.CheckoutGiftMessage.Filters;
 
 namespace Nop.Plugin.Widgets.CheckoutGiftMessage.Infrastructure
@@ -19,6 +21,13 @@ namespace Nop.Plugin.Widgets.CheckoutGiftMessage.Infrastructure
             {
                 options.Filters.Add<CartActionFilter>();
             });
+
+            services.Configure<RazorViewEngineOptions>(o =>
+            {
+                o.ViewLocationExpanders.Add(new GiftMessageSearchViewEngine());
+            });
+
+            services.AddScoped<Nop.Web.Controllers.CheckoutController, BambooCheckoutController>();
         }
 
         public int Order => 3000;
