009dde76547a5b2f83d2469ffdbd760e750df3b2
amingamal <amingamal@hotmail.com>
2025-05-30 08:28:15 +0200

    init Bamboo card Api plugin

---
+++ src/Plugins/Nop.Plugin.Misc.BambooCardApi/BambooCardApiPlugin.cs
@@ -0,0 +1,11 @@
+using Nop.Services.Plugins;
+
+namespace Nop.Plugin.Misc.BambooCardApi
+{
+    /// <summary>
+    /// Rename this file and change to the correct type
+    /// </summary>
+    public class BambooCardApiPlugin : BasePlugin
+    {
+    }
+}
