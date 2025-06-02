f67cd8edc474614ed10d02b1e954875990cedd97
amingamal <amingamal@hotmail.com>
2025-06-02 07:29:44 +0200

    implementing the IBambooCardOrderService interface to fetch order details

---
+++ src/Plugins/Nop.Plugin.Misc.BambooCardApi/Services/BambooCardOrderService.cs
@@ -0,0 +1,23 @@
+﻿using Nop.Core.Domain.Orders;
+using Nop.Data;
+using Nop.Plugin.Misc.BambooCardApi.RequestFeatures;
+
+namespace Nop.Plugin.Misc.BambooCardApi.Services
+{
+    public class BambooCardOrderService : IBambooCardOrderService
+    {
+        private readonly IRepository<Order> _orderRepository;
+
+        public BambooCardOrderService(IRepository<Order> orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<PagedList<Order>> GetOrderDetailsById(int customerId, int pageIndex, int pageSize)
+        {
+            var orders = _orderRepository.Table.Where(o => o.CustomerId == customerId && o.Deleted == false);
+
+            return PagedList<Order>.ToPagedList(orders.OrderBy(o => o.CreatedOnUtc), pageIndex, pageSize);
+        }
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Misc.BambooCardApi/Services/IBambooCardOrderService.cs
@@ -0,0 +1,10 @@
+﻿using Nop.Core.Domain.Orders;
+using Nop.Plugin.Misc.BambooCardApi.RequestFeatures;
+
+namespace Nop.Plugin.Misc.BambooCardApi.Services
+{
+    public interface IBambooCardOrderService
+    {
+        Task<PagedList<Order>> GetOrderDetailsById(int customerId, int pageIndex, int pageSize);
+    }
+}
